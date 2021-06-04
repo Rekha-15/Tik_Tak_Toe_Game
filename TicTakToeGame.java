3599cf3db6c7a5e91ae12591c240a076f05cbdd0
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-04 00:29:01 -0400

    add: TicTakToeGame.java

--- TicTakToeGame.java
+++ TicTakToeGame.java
@@ -1,20 +1,40 @@
 package Com;
+import java.util.Scanner;
 
 public class TicTakToeGame
-{
-
-	public static void main(String[] args) 
-	{
-		System.out.println("Welcome to Tic Tac Toe Game");
-		char[] board = createBoard();
-	}
-	public static char[] createBoard()
-	{
-		char[] board = new char[10];
-		for(int index =0; index < board.length; index++)
+    {
+		public static void main(String[] args) 
 		{
-			board[index] = ' ';
+			System.out.println("Welcome to Tic Tac Toe Game");
+			char UserLetter = chooseLetter();
+			char ComputerLetter ;
+			if(UserLetter == 'X') 
+			{
+				ComputerLetter = '0'; // if user entered value x computerletter will be 0
+			}
+			else
+			{
+				ComputerLetter = 'X'; // if user entered value 0 computerletter will be x
+			}
+			System.out.println("Computer Letter is: " + ComputerLetter + " User Letter is:" + UserLetter); // printing statement 
 		}
-		return board;
+		public static char chooseLetter()
+		{
+			char alphabet;
+			Scanner UserInput = new Scanner(System.in); 
+			// taking input
+			System.out.println("Enter the Alphabet");
+			alphabet=UserInput.next().toUpperCase().charAt(0);
+			return alphabet;
+			
+		}
+		public static char[] createBoard()
+		{
+			char[] board = new char[10]; // creat board
+			for(int index =0; index < board.length; index++)
+			{
+				board[index] = ' ';
+			}
+			return board;
+		}
 	}
-}
